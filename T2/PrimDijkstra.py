a6439f8ff4cc5a2705da413ccaa2e4f9848679ee
DouglasBarbino <douglas-448@hotmail.com>
2017-05-11 19:34:35 -0300

    Leitura dos grafos do T2 completa

--- T2/PrimDijkstra.py
+++ T2/PrimDijkstra.py
@@ -5,14 +5,29 @@ Implementacao dos algoritmos de Prim e Dijkstra
 """
 
 import networkx as nx
+import numpy as np
 import random as rm
 
-def Prim ():
+def Prim (GP, raizGP):
     return
 
 def Dijkstra ():
     return
     
-G = nx.read_edgelist('women.txt', nodetype=int, data=(('weight',float),))
-print(G.nodes())
-print(G.edges(data = True))
+#Leitura de um grafo por meio de uma lista de arestas
+GP = nx.read_edgelist('women.txt', nodetype=int, data=(('weight',float),))
+#print(GP.nodes())
+#print(GP.edges(data = True))
+#Define um vertice inicial aleatorio
+vertices = GP.nodes()
+raizGP = vertices[rm.randint(0,GP.number_of_nodes()-1)]
+#Chama o algoritmo de Prim. 
+#Note que nao eh passada a lista de pesos das arestas como no algoritmo 
+#original, pois ela eh facilmente obtida por GP.get_edge_data(v1, v2)['weight']
+Prim(GP, raizGP)
+
+#Leitura de um grafo por meio de uma matriz de adjacencia
+MAdj = np.loadtxt('cidades.txt')
+GD = nx.from_numpy_matrix(MAdj)
+#print(GD.nodes())
+#print(GD.edges(data = True))
