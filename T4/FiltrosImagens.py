f18254128a7cb51370468e8f0472c58c13510d78
Carol Santos <Carol Santos>
2017-07-12 02:36:54 -0300

    modularizando codigo

--- T4/FiltrosImagens.py
+++ T4/FiltrosImagens.py
@@ -7,9 +7,9 @@ para tratar o ruido impulssivo e o Gaussiano, respectivamente
 import skimage
 import numpy as np
 from skimage import io
-from skimage import exposure
 
-def FiltroMediana(imagem,dimensaoJanela):
+
+def converteNormalizaImagem(imagem):
     #Converte a imagem para float
     imagemFloat = skimage.util.img_as_float(imagem)
     if (imagemFloat.max() != 1.0):
@@ -19,8 +19,11 @@ def FiltroMediana(imagem,dimensaoJanela):
             #Percorre a largura da imagem
             for j in range(0, imagem.shape[1]):
                 imagemFloat[i][j] = imagemFloat[i][j] / valorNormalizacao
+    return imagemFloat
+
+def FiltroMediana(imagem,dimensaoJanela):
+    imagem_normalizada = converteNormalizaImagem(imagem)
     imagemTratada = np.zeros(shape=(imagem.shape[0],imagem.shape[1]))
-    exposure.rescale_intensity(imagemTratada, in_range=(-1, 1))
     pixels = dimensaoJanela*dimensaoJanela
     #Momento de percorrer a imagem para trata-la (loop externo = altura; loop interno = largura)
     for i in range(0, imagem.shape[0]):
@@ -30,14 +33,15 @@ def FiltroMediana(imagem,dimensaoJanela):
                 #- Primeira coordenada lida com a altura, segunda com a largura.
                 #- (dimensaoJanela-1)/2 porque queremos a mesma quantidade de pixels 
                 #anteriores e posteriores a janela, alem do pixel na mesma coordenada
-                elementos_janela.append(imagemFloat[((k//dimensaoJanela)+i-((dimensaoJanela-1)/2))%imagem.shape[0]][((k%dimensaoJanela)+j-((dimensaoJanela-1)/2))%imagem.shape[1]] )
-            
-                #- Primeira coordenada lida com a altura, segunda com a largura.
-                #- (dimensaoJanela-1)/2 porque queremos a mesma quantidade de pixels 
-                #anteriores e posteriores a janela, alem do pixel na mesma coordenada
+                #faz uma lista dos elementos da janela
+                elementos_janela.append(imagem_normalizada[((k//dimensaoJanela)+i-((dimensaoJanela-1)/2))%imagem.shape[0]][((k%dimensaoJanela)+j-((dimensaoJanela-1)/2))%imagem.shape[1]] )
+            #calcula o tamanho da lista dos elementos da janela
             size_elementos = len(elementos_janela)
+            #calcula o indice da mediana
             indiceMediana = size_elementos/2
+            #ordena os elementos por ordem crescente
             elementos_janela.sort()
+            #calcula a mediana
             mediana = elementos_janela[int(indiceMediana)]
             #Atribuicao do pixel tratado na nova imagem
             imagemTratada[i][j] = mediana
@@ -45,16 +49,7 @@ def FiltroMediana(imagem,dimensaoJanela):
             
     
 def FiltroWiener(imagem, dimensaoJanela, sigmaN):
-    #Converte a imagem para float
-    imagemFloat = skimage.util.img_as_float(imagem)
-    #Normalizacao da imagem caso ela nao tenha nenhum pixel branco ([255, 255, 255], 1.0 em float)
-    if (imagemFloat.max() != 1.0):
-        valorNormalizacao = imagemFloat.max()
-        #Percorre a altura da imagem
-        for i in range(0, imagem.shape[0]):
-            #Percorre a largura da imagem
-            for j in range(0, imagem.shape[1]):
-                imagemFloat[i][j] = imagemFloat[i][j] / valorNormalizacao
+    imagem_normalizada = converteNormalizaImagem(imagem)
     #Cria uma matriz vazia que sera a saida
     imagemTratada = np.zeros(shape=(imagem.shape[0],imagem.shape[1]))
     #Armazena o numero de pixels que a janela possui
@@ -70,20 +65,20 @@ def FiltroWiener(imagem, dimensaoJanela, sigmaN):
                 #- Primeira coordenada lida com a altura, segunda com a largura.
                 #- (dimensaoJanela-1)/2 porque queremos a mesma quantidade de pixels 
                 #anteriores e posteriores a janela, alem do pixel na mesma coordenada
-                miF += (1/pixels) * imagemFloat[((k//dimensaoJanela)+i-((dimensaoJanela-1)/2))%imagem.shape[0]][((k%dimensaoJanela)+j-((dimensaoJanela-1)/2))%imagem.shape[1]] 
+                miF += (1/pixels) * imagem_normalizada[((k//dimensaoJanela)+i-((dimensaoJanela-1)/2))%imagem.shape[0]][((k%dimensaoJanela)+j-((dimensaoJanela-1)/2))%imagem.shape[1]] 
             for k in range (pixels):
-                sigmaF += (1/pixels) * (imagemFloat[((k//dimensaoJanela)+i-((dimensaoJanela-1)/2))%imagem.shape[0]][((k%dimensaoJanela)+j-((dimensaoJanela-1)/2))%imagem.shape[1]] - miF)**2
+                sigmaF += (1/pixels) * (imagem_normalizada[((k//dimensaoJanela)+i-((dimensaoJanela-1)/2))%imagem.shape[0]][((k%dimensaoJanela)+j-((dimensaoJanela-1)/2))%imagem.shape[1]] - miF)**2
             #Modifica sigmaS caso sigmaF - sigmaN sejam maiores do que 0, 
             #caso contrario fica o valor limpo anteriormente
             if (sigmaF - sigmaN > 0):
                 sigmaS = sigmaF - sigmaN
             #Atribuicao do pixel tratado na nova imagem
-            imagemTratada[i][j] = miF + (sigmaS * (imagemFloat[i][j] - miF) / (sigmaS + sigmaN))
+            imagemTratada[i][j] = miF + (sigmaS * (imagem_normalizada[i][j] - miF) / (sigmaS + sigmaN))
     return imagemTratada
     
 #Carrega a imagem
-imagem1 = io.imread('imagens_ruidosas\camera_gaussian.png')
-imagem2= io.imread('imagens_ruidosas\camera_sal_e_pimenta.png')
+imagem1 = io.imread('imagens_ruidosas\\camera_gaussian.png')
+imagem2= io.imread('imagens_ruidosas\\camera_sal_e_pimenta.png')
 
 '''--Testes--'''
 #Coordenadas
@@ -101,4 +96,4 @@ imagem1Tratada = FiltroWiener(imagem1, 5, 0.01)
 io.imsave('imagens_ruidosas\camera_gaussian_tratada5.png', imagem1Tratada)
 imagem2Tratada = FiltroMediana(imagem2,3)
 #Salvar imagem tratada
-io.imsave('imagens_ruidosas\camera_sal_e_pimenta_tratada.png', imagem2Tratada)
+io.imsave('imagens_ruidosas\\camera_sal_e_pimenta_tratado.png', imagem2Tratada)
